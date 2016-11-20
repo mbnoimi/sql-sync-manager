9eabe17123c27363914fa7d6ff4160b999d362ce
mbnoimi <mbnoimi@gmail.com>
2016-11-20 20:38:01 +0300

    Remove unused lib files

--- deb/fix_executable.sh
+++ deb/fix_executable.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 appname="$1"
 qt="$2"
 binpath="$3"
@@ -12,13 +13,20 @@ chrpath -d $binpath/$appname
 #### Create files tree ####
 rm -fr $project_path/deb/files
 mkdir -p $project_path/deb/files
-cd $distro
-mkdir bin
-mkdir -p libs/plugins
+mkdir -p $distro/bin
+mkdir -p $distro/libs/plugins
 cp -f $binpath/$appname $distro/bin/$appname
-chmod +x  $distro/bin/$appname
+chmod +x $distro/bin/$appname
 cp -f $project_path/deb/$appname.sh $distro/$appname.sh
-chmod +x  $distro/$appname.sh
+chmod +x $distro/$appname.sh
 cp -f $project_path/deb/qt.conf $distro/bin/qt.conf
 cp -fr $qt/lib/* $distro/libs
 cp -fr $qt/plugins $distro/libs
+rm -fr $distro/libs/cmake/ $distro/libs/pkgconfig/
+shopt -s extglob
+cd $distro/libs/plugins/
+rm -fr !(sqldrivers|platforms)
+cd $distro/libs/
+rm -fr !(plugins|libicudata.so*|libicui18n.so*|libicuuc.so.56*|libqgsttools_p.so*|libQt5Core.so*|libQt5Sql.so*)
+
+
